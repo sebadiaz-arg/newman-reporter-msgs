4c056329fc4b714b5db4e4d91094115f16551e8d
Roberto Mier Escandon <rmier@tuenti.com>
2020-03-06 11:16:57 +0100

    Initial code

---
+++ README.md
@@ -0,0 +1,36 @@
+# Newman reporter msgs
+
+A Newman reporter that dumps to stdout the raw http messages involved in
+the newman run execution
+
+## Hack
+
+### Install nvm and node
+```sh
+curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh -o install_nvm.sh
+sh install_nvm.sh
+source ~/.profile
+nvm install -lts
+```
+
+### Install globally newman
+```sh
+npm i -g newman
+```
+
+### Pack from sources and deploy
+```sh
+npm pack
+npm i -g newman-reporter-msgs-<version>.tgz
+```
+
+## Execute newman with this reporter
+```sh
+newman run -e <path/to/environment.json> <path/to/collection.json> -r msgs
+```
+
+The resultant http messages exchanges will be shown in console
+
+### Options
+You can use the flag *--reporter-msgs-native* to use the OS native 
+line break instead of the default CRLF (\r\n)
---
+++ index.js
@@ -0,0 +1,85 @@
+os = require('os')
+
+var CRLF = '\r\n';
+
+function line(what) {
+  if (!what) {
+    return CRLF
+  }
+  return what + CRLF
+}
+
+/**
+ * Reporter that outputs logs to Google Drive Spreadsheet.
+ *
+ * @param {Object} newman - The collection run object, with event hooks for reporting run details.
+ * @param {Object} reporterOptions - The set of options received by this reporter by cli argument
+ * @param {Object} options - A set of collection run options.
+ * @param {String=} options.native - Optional flag to use native crlf
+ * @returns {*}
+ */
+module.exports = function newmanMsgsReporter(newman, reporterOptions, options) {
+  newman.on('start', (err, args) => {
+    if (reporterOptions.native) {
+      CRLF = os.EOL
+    }
+  })
+  newman.on('request', (err, args) => {
+    if (err) return
+
+    let str = ""
+    let req_method = args.request.method
+
+    let req_host = args.request.url.host.join(".")
+    if (args.request.url.port) {
+      req_host = req_host + ":" + args.request.url.port
+    }
+
+    let req_path = args.request.url.path.join("/")
+
+    let req_first_line = req_method + " /" + req_path + " HTTP/1.1"
+
+    let req_headers = args.request.headers.members
+    let req_body = ""
+    if (args.request.body) {
+      req_body = args.request.body.raw
+    }
+
+    let res_first_line = "HTTP/1.1 " + args.response.code + " " + args.response.status
+    let res_headers = args.response.headers.members
+    let res_body = ""
+    if (req_method === 'POST' || req_method === 'PUT' || req_method === 'PATCH') {
+      let rawBody = args.response.stream;
+      res_body = rawBody.toString();
+    }
+
+    str += line(req_first_line)
+    req_headers.forEach(h => {
+      str += line(h.key + ": " + h.value)
+    });
+
+    if (req_body) {
+      str += line()
+      str += line(req_body)
+    }
+
+    str += line()
+    str += line(res_first_line)
+    res_headers.forEach(h => {
+      str += line(h.key + ": " + h.value)
+    });
+
+    if (res_body) {
+      str += line()
+      str += line(res_body)
+    }
+
+    str += line()
+    str += line("-----------------------------------------------------------------")
+    str += line()
+
+    console.log(str)
+  })
+}
+
+
